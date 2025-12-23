507d74577c8cf3268c4172645425377152abf1a6
aaronsood <theaaronsood@gmail.com>
2025-12-24 03:51:35 +1300

    small tweaks to rom.py

--- rom.py
+++ rom.py
@@ -1,11 +1,15 @@
-# coded by aaronsood
 class ROM:
     def __init__(self, path):
+        # read entire rom into memory
         with open (path, "rb") as f:
          self.data = f.read()
 
+        # header values from gb rom spec
         self.title = self.data[0x134:0x144].decode("ascii", errors="ignore").strip()
         self.cartridge_type = self.data[0x147]
         self.rom_size = self.data[0x148]
+
     def get_byte(self, address):
+       if address < 0 or address >= len(self.data):
+          return 0xFF
        return self.data[address]
