d02414cd5827113899c01d914b7cdf2a8b8da17c
aaronsood <theaaronsood@gmail.com>
2025-12-24 04:09:41 +1300

    added comments and spaces in between lines for readability

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,12 +1,22 @@
 import sys
 import os
+
+# add parent directory to path so we can import rom and memory
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from rom import ROM
 from memory import Memory
+
+# load test rom and memory
 rom = ROM("tests/cpu_instrs.gb")
 mem = Memory(rom)
+# test rom read
 print("ROM byte @ 0x100:", hex(mem.read(0x100)))
+
+# test writing to work RAM
 mem.write(0xC000, 0x42)
 print("WRAM byte @ 0xC000:", hex(mem.read(0xC000)))
+
+# test writing to high RAM
 mem.write(0xFF80, 0x99)
 print("HRAM byte @ 0xFF80:", hex(mem.read(0xFF80))) 
