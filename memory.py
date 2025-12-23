33a74bbaf2439f3474f77cc1e849e8f47079f962
aaronsood <theaaronsood@gmail.com>
2025-12-24 03:57:20 +1300

    fixed a typo, added some comments and debugging features

--- memory.py
+++ memory.py
@@ -1,9 +1,15 @@
 class Memory:
     def __init__(self, rom):
+        # rom is loaded from ROM class
         self.rom = rom
+        # work ram 
         self.wram = bytearray(0x2000)
+        # high ram
         self.hram = bytearray(0x7F)
+        self.debug = False
+
     def read(self, addr: int) -> int:
+        # read from rom, wrom, hram
         if 0x0000 <= addr <= 0x7FFF:
             return self.rom.get_byte(addr)
         elif 0xC000 <=addr <= 0xDFFF:
@@ -12,9 +18,14 @@ class Memory:
             return self.hram[addr - 0xFF80]
         else:
             return 0xFF
+
     def write(self, addr: int, value: int):
+        # mask value to 8 bits before writing
         value &= 0xFF
-        if 0xC000 <= addr <= 0xDFF:
+        if 0xC000 <= addr <= 0xDFFF:
             self.wram[addr - 0xC000] = value
         elif 0xFF80 <= addr <= 0xFFFE:
             self.hram[addr - 0xFF80] = value
+
+        if self.debug:
+            print(f"write{hex(value)} to {hex(addr)}")
