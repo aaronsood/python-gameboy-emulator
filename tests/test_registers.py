2cac682d969a4fd873ff9485f40889e8ecbf851c
aaronsood <theaaronsood@gmail.com>
2025-12-24 04:16:19 +1300

    added some comments and spacing for easier readability

--- tests/test_registers.py
+++ tests/test_registers.py
@@ -1,19 +1,36 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  
+
+# add parent folder to path to import registers
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from registers import Registers
+
+# make a registers instance
 r = Registers()
-r.BC = 0x1234
+r.debug = True # optional debug prints
+
+# test BC register
+r.BC = 0x1234  
 print(f"BC: {hex(r.BC)}, B: {hex(r.B)}, C: {hex(r.C)}")
 assert r.B == 0x12
 assert r.C == 0x34
 assert r.BC == 0x1234
+
+# test AF register
 r.A = 0x56
 r.F = 0xF0
 assert r.A == 0x56
 assert r.F == 0xF0
 assert r.AF == 0x56F0
+
+# test HL register
 r.HL = 0xABCD
 assert r.H == 0xAB
 assert r.L == 0xCD
-print("All register tests passed.")
+
+# optional casual debug   
+if r.debug:
+    print("Registers tests done.")
+    
+print ("All registers passed.")
