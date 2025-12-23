eb0ed3e74588584883dd37e36b94f9a8106daf90
aaronsood <theaaronsood@gmail.com>
2025-12-24 09:59:20 +1300

    add cpu skeleton to project

---
+++ cpu.py
@@ -0,0 +1,29 @@
+class CPU:
+    def __init__(self, memory):
+        # store memory instance
+        self.mem = memory
+        # registers will also be connected later
+        self.reg = None
+        self.halted = False # cpu is running by default
+
+    def connect_registers(self, registers):
+        # attach registers to cpu
+        self.reg = registers
+    
+    def fetch_byte(self):
+        # read byte from memory at pc and increment pc
+        pc = self.reg.pc
+        val = self.mem.read(pc)
+        self.reg.PC += 1
+        return val
+    
+    # placeholder for instruction execution
+    def execute_next(self):
+    # fetch opcode
+        opcode = self.fetch_byte()
+        # for now, just handle nop (0x00)
+        if opcode == 0x00:
+            pass # do nothing
+        else:
+            # simple placeholder for unimplemented opcodes
+            print(f"opcode {hex(opcode)} not implemented yet")
