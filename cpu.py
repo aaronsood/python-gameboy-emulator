314688255817243ed3a566d9ae4039c2969bc5fc
aaronsood <theaaronsood@gmail.com>
2025-12-27 20:46:42 +1300

    add opcodes to run proper classical games such as tetris

--- cpu.py
+++ cpu.py
@@ -161,6 +161,177 @@ class CPU:
             self.reg.PC += offset
             return
         
+        # INC (HL)
+        if opcode == 0x34:
+            val = (self.mem.read(self.reg.HL) + 1) & 0xFF
+            self.mem.write(self.reg.HL, val)
+            return
         
-        # unimplemented opcode
-        raise NotImplementedError(f"Opcode {opcode:#02x} not implemented") 
+        # DEC (HL)
+        if opcode == 0x35:
+            val = (self.mem.read(self.reg.HL) - 1) & 0xFF
+            self.mem.write(self.reg.HL, val)
+            return
+        
+        # LD A,(HL)
+        if opcode == 0x7E: 
+            self.reg.A = self.mem.read(self.reg.HL)
+            return
+        
+        # LD (HL), A
+        if opcode == 0x77:
+            self.mem.write(self.reg.HL, self.reg.A)
+            return
+
+        # LD rr,nn (16-bit immediate loads)
+        ld_rr_imm = {
+            0x01: "BC",
+            0x11: "DE",
+            0x21: "HL",
+            0x31: "SP",
+        }
+        if opcode in ld_rr_imm:
+            val = self.fetch_word()
+            setattr(self.reg, ld_rr_imm[opcode], val)
+            return
+        
+        # LD (HL+),A
+        if opcode == 0x22:
+                self.mem.write(self.reg.HL, self.reg.A)
+                self.reg.HL = (self.reg.HL + 1) & 0xFFFF
+                return
+        # LD (HL-), A
+        if opcode == 0x32:
+            self.mem.write(self.reg.HL, self.reg.A)
+            self.reg.HL = (self.reg.HL - 1) & 0xFFFF
+        
+        # LD A,(BC)
+        if opcode == 0x0A:
+            self.reg.A = self.mem.read(self.reg.BC)
+            return
+        
+        # LD A,(DE)
+        if opcode == 0x1A:
+            self.reg.A = self.mem.read(self.reg.DE)
+        
+        # LD A,(nn)
+        if opcode == 0xFA:
+            addr = self.fetch_word()
+            self.reg.A = self.mem.read(addr)
+            return
+        
+        # LD (nn),A
+        if opcode == 0xEA:
+            addr = self.fetch_word()
+            self.mem.write(addr, self.reg.A)
+            return
+        
+        # INC rr
+        inc_rr = {
+            0x03: "BC",
+            0x13: "DE",
+            0x23: "HL",
+            0x33: "SP"
+        }
+        if opcode in inc_rr:
+            name = inc_rr[opcode]
+            setattr(self.reg, name, (getattr(self.reg, name) + 1) & 0xFFFF)
+            return
+        
+        # DEC rr
+        dec_rr = {
+            0x0B: "BC",
+            0x1B: "DE",
+            0x2B: "HL",
+            0x3B: "SP"
+        }
+        if opcode in dec_rr:
+            name = dec_rr[opcode]
+            setattr(self.reg, name, (getattr(self.reg, name) -1) & 0xFFFF)
+            return  
+        
+        # ADD HL,rr
+        add_hl = {
+            0x09: "BC",
+            0x19: "DE",
+            0x29: "HL",
+            0x39: "SP"
+        }
+        if opcode in add_hl:
+            val = getattr(self.reg, add_hl[opcode])
+            self.reg.HL = (self.reg.HL + val) & 0xFFFF
+            return
+        
+        # XOR A
+        if opcode == 0xAF:
+            self.reg.A = 0
+            return
+        
+        # AND n
+        if opcode == 0xE6:
+            self.reg.A &= self.fetch_byte()
+            return
+        
+        # OR n
+        if opcode == 0xF6:
+            self.reg.A |= self.fetch_byte()
+            return
+        
+        # JR Z,n
+        if opcode == 0x28:
+            off = self.fetch_byte()
+            if self.reg.Z: # if zero flag set
+                if off >= 0x80: off -= 0x100
+                self.reg.PC = (self.reg.PC+ off) & 0xFFFF
+                return
+            
+        # JR NZ,n
+        if opcode == 0x20:
+            off = self.fetch_byte()
+            if not self.reg.Z:
+                if off >= 0x80: off -= 0x100
+                self.reg.PC = (self.reg.PC + off) & 0xFFFF
+                return
+            
+        # PUSH rr
+        push_rr = {
+            0xC5: "BC",
+            0xD5: "DE",
+            0xE5: "HL",
+            0xF5: "AF"
+        }
+        if opcode in push_rr:
+            val = getattr(self.reg, push_rr[opcode])
+            self.reg.SP = (self.reg.SP - 2) & 0xFFFF
+            self.mem.write_word(self.reg.SP, val)
+            return
+        
+        # POP rr
+        pop_rr = { 
+            0xC1: "BC",
+            0xD1: "DE",
+            0xE1: "HL",
+            0xF1: "AF"
+        }
+        if opcode in pop_rr:
+            val = self.mem.read_word(self.reg.SP)
+            self.reg.SP = (self.reg.SP + 2) & 0xFFFF
+            setattr(self.reg, pop_rr[opcode], val)
+            return
+        
+        # CALL nn
+        if opcode == 0xCD:
+            addr = self.fetch_word()
+            self.reg.SP = (self.reg.SP -2) & 0xFFFF
+            self.mem.write_word(self.reg.SP, self.reg.PC)
+            self.reg.PC = addr
+            return
+        
+        # RET
+        if opcode == 0xC9:
+            self.reg.PC = self.mem.read_word(self.reg.SP)
+            self.reg.SP = (self.reg.SP + 2) & 0xFFFF
+            return
+        
+            # unimplemented opcode
+            raise NotImplementedError(f"Opcode {opcode:#02x} not implemented") 
