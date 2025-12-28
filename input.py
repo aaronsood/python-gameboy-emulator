075c989bc90aba8cad33df3bc91b7679e72a6fe7
aaronsood <theaaronsood@gmail.com>
2025-12-28 22:56:09 +1300

    add input handling for runnning roms

--- input.py
+++ input.py
@@ -23,28 +23,30 @@ BUTTONS = {
 
 def poll_input(memory):
     # 0 means pressed 1 = released
+    pygame.event.pump
     keys = pygame.key.get_pressed()
 
     for key, name in KEY_MAP.items():
         BUTTONS[name] = 0 if keys[key] else 1
 
-    reg = memory.read(0xFF00)
+    joyp = memory.read(0xFF00)
 
-    p14 = not (reg & 0x10)
-    p15 = not (reg & 0x20)
+    select_dpad = not (joyp & 0x10)
+    select_buttons = not (joyp & 0x20)
     
+
     val = 0xFF
 
-    if p14:
+    if select_dpad:
         val &= ~(BUTTONS['RIGHT'] << 0)
         val &= ~(BUTTONS['LEFT'] << 1)
         val &= ~(BUTTONS['UP'] << 2)
         val &= ~(BUTTONS['DOWN'] << 3)
 
-    if p15:
+    if select_buttons:
         val &= ~(BUTTONS['A'] << 0)
         val &= ~(BUTTONS['B'] << 1)
         val &= ~(BUTTONS['SELECT'] << 2)
         val &= ~(BUTTONS['START'] << 3)
     
-    memory.write(0xFF00, val)g
+    memory.write(0xFF00, val)
